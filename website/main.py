7f6055fca797cfc7565a0b2d1445e09912be7804
Acudo <dominic@acuna.id.au>
2024-05-13 14:37:58 +1000

    something

--- website/main.py
+++ website/main.py
@@ -1,25 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Blueprint
+from . import db
 
-app = Flask(__name__)
+main = Blueprint('main', __name__)
 
-
-@app.route('/')
+@main.route('/')
 def index():
-    return render_template('index.html')
+    return 'Index'
 
-
-@app.route('/process_data', methods=['POST'])
-def process_data():
-    input_data = request.form['input_data']
-    with open("returned_inputs.txt", "a") as f:
-        f.write(input_data + '\n')
-    return 'Data processed: ' + input_data
-
-
-@app.route('/update_streak', methods=['POST'])
-def update_streak():
-    print("hello")
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@main.route('/profile')
+def profile():
+    return 'Profile'
